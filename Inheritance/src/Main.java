f3c760781d03dc59f56ccaf21af6fb0ae6adcc90
kingbaldwin4 <kingbaldwinfour@gmail.com>
2022-09-22 19:27:57 +0300

    first commit

---
+++ Inheritance/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer custoer = new Customer();
+		Employee employee = new Employee();
+		EmployeeManager employeeManager = new EmployeeManager();
+		CustomerManager customerManager = new CustomerManager();
+		
+
+	}
+
+}
